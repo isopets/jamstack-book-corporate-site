6ace5542e99a8ce5e77473f2bf505d9c23520c64
yuto isogai <jandp.0717@gmail.com>
2022-06-19 09:57:09 +0900

    favicon add

---
+++ .history/src/templates/single-blog_20220619095529.js
@@ -0,0 +1,59 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
+import * as style from "../styles/singleBlog.module.scss"
+import Seo from "../components/seo"
+const SingleBlog = props => {
+  return (
+    <Layout>
+      <Seo
+        title={props.data.contentfulBlog.title}
+        description={props.data.contentfulBlog.excerpt}
+      />
+      <div className={style.hero}>
+        <GatsbyImage
+          image={
+            props.data.markdownRemark.contentfulBlog.childImageSharp
+              .gatsbyImageData
+          }
+          alt="blog-image"
+        />
+      </div>
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <h1>{props.data.contentfulBlog.title}</h1>
+          <p>{props.data.contentfulBlog.date}</p>
+          <div
+            dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+          />
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default SingleBlog
+
+export const query = graphql`
+  query ContentfulSingleBlogQuery($slug: String!) {
+    contentfulBlog(slug: { eq: $slug }) {
+      title
+      excerpt
+      date(formatString: "YYYY-MM-DD")
+      textBody {
+        childMarkdownRemark {
+          html
+        }
+      }
+      image {
+        gatsbyImageData(
+          formats: AUTO
+          quality: 90
+          placeholder: BLURRED
+          width: 1000
+        )
+      }
+    }
+  }
+`
